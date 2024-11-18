fce0a2ae26334ac44efb8bb2f4f65d1f0b48192b
r. ávila <avila.dev@outlook.com>
2024-11-18 19:47:46 -0300

    feat: add utility funcs

--- result/result.go
+++ result/result.go
@@ -9,8 +9,8 @@ func (r R[T]) Get() T {
 	return r.Value
 }
 
-func (r R[T]) Error() error {
-	return r.Err
+func (r R[T]) Error() string {
+	return r.Err.Error()
 }
 
 func (r R[T]) HasError() bool {
@@ -19,6 +19,26 @@ func (r R[T]) HasError() bool {
 
 func (r R[T]) HasErr() bool {
 	return r.HasError()
+}
+
+func (r R[T]) IsSuccess() bool {
+	return !r.HasError()
+}
+
+func (r R[T]) Unwrap() T {
+	if r.HasError() {
+		panic(r.Error())
+	}
+
+	return r.Value
+}
+
+func (r R[T]) Expect(msg string) T {
+	if r.HasError() {
+		panic(msg)
+	}
+
+	return r.Value
 }
 
 func Of[T any](v T, err error) R[T] {
